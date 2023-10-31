885463b87f242234e1527bcc786787b7a29b3a80
Dh0m16 <144840435+Dh0m16@users.noreply.github.com>
2023-10-31 17:45:16 +0300

    Create db.php

---
+++ inc/db.php
@@ -0,0 +1,8 @@
+<?php 
+
+$conn = mysqli_connect('localhost','root','root','Project_abdalrhman_PHP-JavaScript-Bootstrap-and-MYSQL');
+
+if(!$conn) { echo 'Error: ' , mysqli_connect_error(); }
+
+
+?>
